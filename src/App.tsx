ceef6bbd57132a019bc71ac3606a48621566e227
Jack Mueller <mueller.o.jack@gmail.com>
2019-11-02 23:52:19 -0400

    Add BEM utility. Ensure absolute imports to utility work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./header/Header";
-
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <Header />
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://github.com/LiveSatomi/jackomueller"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Source Code
-                </a>
-            </header>
-        </div>
-    );
+    return <Header />;
 };
 
 export default App;
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Header.scss";
+import bemNames from "util/bemnames";
 
 class HeaderProps {}
+const bem = bemNames.create("Header");
 
 class Header extends React.Component<HeaderProps> {
-    // bah bah b
     render() {
         return (
-            <div>
-                <p className={"Header"}>hello</p>
+            <div className={bem.b()}>
+                <p className={bem.e("message")}>hello</p>
             </div>
         );
     }
