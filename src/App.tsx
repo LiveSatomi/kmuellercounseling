1ceb6c208f8934d27887ace1a191a1fd84401044
Jack Mueller <mueller.o.jack@gmail.com>
2019-11-04 22:59:47 -0500

    Link wordmark in header (with placeholder image).

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.scss";
-import Header from "./header/Header";
+import Header from "./components/header/Header";
+import wordmark from "brand/wordmark.png";
+
 const App: React.FC = () => {
-    return <Header />;
+    return <Header wordmark={wordmark} />;
 };
 
 export default App;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import "./Header.scss";
 import bemNames from "util/bemnames";
 
-class HeaderProps {}
+type HeaderProps = {
+    wordmark: string;
+};
+
 const bem = bemNames.create("Header");
 
 class Header extends React.Component<HeaderProps> {
     render() {
         return (
             <div className={bem.b()}>
-                <p className={bem.e("message")}>hello</p>
+                <img
+                    className={bem.e("message")}
+                    alt={"JM"}
+                    src={this.props.wordmark}
+                />
             </div>
         );
     }
