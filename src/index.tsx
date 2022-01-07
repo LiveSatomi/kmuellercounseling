09ddba98b82fc3cc136dbd3f6da3d197bff5a35f
Jack Mueller <mueller.o.jack@gmail.com>
2022-01-06 22:11:26 -0500

    Remove log statements.

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,8 @@ import App from "./App";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
-console.log("jmtest");
-console.log("jmtest1 " + document.location.hash);
-console.log("jmtest2 " + document.location);
 const path = (/#!(\/.*)$/.exec(document.location.hash) || [])[1];
-console.log("jmtestA " + path);
 if (path) {
-    console.log("jmtestB");
     history.replace(path);
 }
 
