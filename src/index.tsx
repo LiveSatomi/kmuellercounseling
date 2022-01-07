127f50185cfaed07491ae4bce25f173e3731b491
Jack Mueller <mueller.o.jack@gmail.com>
2022-01-06 20:38:45 -0500

    Trying hash path rewriting method to fix blank homepage/ access denied on sibling pages.

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+const path = (/#!(\/.*)$/.exec(document.location.hash) || [])[1];
+if (path) {
+    history.replace(path);
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
